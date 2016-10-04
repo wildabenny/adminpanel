bba95566d2de70c94dc2359fb905e795a4f3cf24
wildabenny <strzelczykster@gmail.com>
2016-10-04 16:08:14 +0200

    after work tuesday

--- resources/views/editproduct.blade.php
+++ resources/views/editproduct.blade.php
@@ -7,17 +7,17 @@
         {{csrf_field()}}
         {{method_field('PATCH')}}
 
-            <div class="form-group">
-                <textarea name="shortname" class="form-control">{{$product->shortname}}</textarea>
-            </div>
+        <div class="form-group">
+            <textarea name="shortname" class="form-control">{{$product->shortname}}</textarea>
+        </div>
 
-            <div class="form-group">
-                <textarea name="longname" class="form-control">{{$product->longname}}</textarea>
-            </div>
+        <div class="form-group">
+            <textarea name="longname" class="form-control">{{$product->longname}}</textarea>
+        </div>
 
-            <div class="form-group">
-                <textarea name="description" class="form-control">{{$product->description}}</textarea>
-            </div>
+        <div class="form-group">
+            <textarea name="description" class="form-control">{{$product->description}}</textarea>
+        </div>
 
         <div class="form-group">
             <input type="file" name="image" id="image">
