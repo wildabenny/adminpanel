1fbfeceb21a65dc33cde76704fdceba1a03da290
wildabenny <strzelczykster@gmail.com>
2016-10-06 15:53:54 +0200

    thursday stuff

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Auth;
+
+class AdminController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        if (Auth::guest()) {
+            return view('auth.login');
+        }
+        return view('administrator.welcome');
+    }
+
+}
